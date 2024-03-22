99285d0504d788b6d92e2b6275682f536342829c
David Chen <davidc3287@gmail.com>
2024-03-21 20:14:28 -0700

    better catalog description

--- api/main.py
+++ api/main.py
@@ -4,8 +4,10 @@ from .routers import catalog, profiles
 tags_metadata = [
     {
         'name': 'catalog',
-        'description': '''
-These are items ''',
+        'description': """
+The catalog backend component is responsible for retrieving listings from the database given frontend searches and filters. These listings are then displayed to the user on the frontend.
+
+It also handles when a user chooses to purchase an item, working with the profiles backend to show the seller's contact information.""",
     }
 ]
 
