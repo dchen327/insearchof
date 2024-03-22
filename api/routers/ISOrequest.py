aff1bf22a60a3d2f1b900ae61c72dbff319dfdb5
briancha5431 <briancha5431@gmail.com>
2024-03-22 10:35:44 -0700

    isorequest docs mostly done

--- api/routers/ISOrequest.py
+++ api/routers/ISOrequest.py
@@ -9,7 +9,7 @@ router = APIRouter(
 )
 
 
-class ISOrequester(BaseModel):
+class ISOrequesterFilters(BaseModel):
     """
     The ISORequester backend component allows users to request items or services through
     the application, functioning similarly to current ISO management practices.
@@ -42,20 +42,56 @@ class ISOrequester(BaseModel):
         None, description="An image representing the item or service.")
     price: float = Field(
         0.0, description="The price the requester is willing to pay. Must be non-negative.")
+    
+
+class ISOrequester(BaseModel):
+    pass
+
+
+class UploadRequestResponse(BaseModel):
+    """
+    Response model for uploading a request to the database.
+    """
+
+    message: str = Field(
+        ..., description="Response message confirming the request has been uploaded.")
+    
+
+class MarkTransactionRequest(BaseModel):
+    """
+    Model for marking a transaction as complete in the database.
+    """
+    
+    item_id: str = Field(...,
+                         description="The ID of the item being requested.")
+    requester_id: str = Field(..., description="The requester's email.")
+    seller_id: str = Field(..., description="The seller's email.")
+    
+    
+class MarkTransactionCompleteResponse(BaseModel):
+    """
+    Response model for marking a transaction as complete in the database.    
+    """
+    
+    message: str = Field(
+        ..., description="Response message confirming the transaction has been marked as complete.")
 
 
 @router.get("/upload")
-def upload_request():
+def upload_request(filters: ISOrequesterFilters) -> UploadRequestResponse:
     ''' 
     Uploads the request to the database, making it visible to other users 
     and recording the details for transaction history. 
     '''
+    
     return {"message": "Request uploaded"}
 
+
 @router.get("/mark")
-def mark_transaction_complete():
+def mark_transaction_complete(mark_request: MarkTransactionRequest) -> MarkTransactionCompleteResponse:
     '''
     Marks the associated transaction as complete in the database. This is used
     to update the transaction history once a successful transaction has been completed.
     '''
+    
     return {"message": "Transaction marked complete"}
