deb94fe896b77f376cecd9316a61a57d5ee3851f
Arushi Malik <arushi.malik@audintel.in>
2024-03-25 23:33:44 -0700

    updated main.py

--- api/main.py
+++ api/main.py
@@ -6,7 +6,6 @@ tags_metadata = [
         'name': 'catalog',
         'description': """
 The catalog backend component is responsible for retrieving listings from the database given frontend searches and filters. These listings are then displayed to the user on the frontend.
-
 It also handles when a user chooses to purchase an item, working with the profiles backend to show the seller's contact information.""",
     },
     {
@@ -18,7 +17,8 @@ It also handles when a user chooses to purchase an item, working with the profil
         'name': 'sellList',
         'description': """The sellList backend component is responsible for creating selling and listing functionality within our marketplace.
          Users are able to do the following: add, update, and delete posts."""
-
+    },
+    {
          'name': 'Profiles',
         'description': """The User backend component allows users users to view and manage their profile information, view transaction history,
     and interact with the platform.
