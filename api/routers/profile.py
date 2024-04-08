34876e010c9c72323cabf6ac57f848751cb3f603
henry-trinh <htrinh@g.hmc.edu>
2024-04-07 22:01:33 -0700

    Arushi's changes for Profiles, based on post-review

--- api/routers/profile.py
+++ api/routers/profile.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from typing import Optional, List
-from pydantic import BaseModel, Field
+from typing import Optional, List 
+from pydantic import BaseModel, Field, constr
 
 router = APIRouter(
     prefix='/profile',
@@ -15,9 +15,9 @@ class UserProfile(BaseModel):
     Attributes:
         email (str): The user's email.
         name (str): The user's name.
-        profile_picture (str, optional): A profile picture representing the user.
+        profile_picture (str, optional): An image of a profile picture representing the user.
         location (str): The user's location on campus specifically.
-        phone_number (str, optional): The user's phone number (not necessary; can be optional).
+        phone_number (regex, optional): The user's phone number (not necessary; can be optional).
 
     Methods:
         upload_contact_info(): Uploads the users contact information to the database.
@@ -28,22 +28,10 @@ class UserProfile(BaseModel):
 
     email: str = Field(None, description="The user's email.")
     name: str = Field(None, description="The user's name.")
-    profile_picture: Optional[str] = Field(None, description="A profile picture representing the user.")
+    profile_picture: Optional[str] = Field(None, description="An image of the profile picture representing the user.")
     location: str = Field(None, description="The user's location on campus specifically")
-    phone_number: Optional[str] = Field(None, description="The user's phone number")
+    phone_number: Optional[constr(regex=r'^\(\d{3}\)\s\d{3}-\d{4}$')] = Field(None, description="The user's phone number")
     
-    def __init__(self, email, name, profile_picture=None, location=None, phone_number=0):
-        """
-        Initializes a new User Profile instance with the given parameters.
-
-        Parameters:
-            email (str): The user's email.
-            name (str): The user's name.
-            profile_picture (Image, optional): A profile picture representing the user.
-            location (str): The user's location on campus specifically).
-            phone_number (int, optional): The user's phone number (not necessary; can be optional).
-        """
-
 
     class UploadContactInfoResponse(BaseModel):
         """
@@ -59,10 +47,7 @@ class UserProfile(BaseModel):
         Model for getting a list of items from the database.
         """
 
-        user_id: str = Field(...,
-                         description="The ID of the user whose list of items is being requested.")
         requester_id: str = Field(..., description="The requester's email.")
-        # seller_id: str = Field(..., description="The seller's email.")
 
 
     class GetListOfItemsResponse(BaseModel):
@@ -70,66 +55,47 @@ class UserProfile(BaseModel):
         Response model for getting a list of items from the database.    
         """
 
-        listingOfItems: List[list] = Field(
-            ..., description="Response message confirming the user's list of items has been requested.")
+        listingOfItems: List[dict] = Field(
+            ..., description="List of items associated with the user.")
+
 
     class GetTransactionHistoryRequest(BaseModel):
-        """
-        Model for getting a user's transaction history from the database.
-        """
-
-        user_id: str = Field(...,
-                         description="The ID of the user whose transaction history is being requested.")
+        pass
 
     class GetTransactionHistoryResponse(BaseModel):
         """
         Response model for getting a user's transaction history in the database.    
         """
 
-        listingOfTransactionHistory: List[list] = Field(
-            ..., description="Response message confirming the user's transaction history been requested.")
+        listingOfTransactionHistory: List[dict] = Field(
+            ..., description="User's transaction history.")
 
 
     @router.put("/upload_contact_info")
-    def upload_contact_info() -> UploadContactInfoResponse:
+    def upload_contact_info(user_profile: UserProfile) -> UploadContactInfoResponse:
         """
         Uploads a users contact information, including their name, email, profile picture, optional
         phone number, to the user database, making it visible to buyers.
         """
         return {"message": "Uploads users contact info successfully"}
 
-    @router.get("/get_list_of_items/{user_id}")
-    def get_list_of_items(get_list: GetListOfItemsRequest, user_id: str) -> GetListOfItemsResponse:
+
+    @router.get("/get_list_of_items")
+    def get_list_of_items(get_list: GetListOfItemsRequest) -> GetListOfItemsResponse:
         """
         Retrieves a list of items associated with a given user from the itemsForSale and itemsForRent 
         database and stores it within the user database, making it visible to buyers. 
         This allows potential buyers to view items listed by the user.
 
-        Parameters:
-            user_id (str): The unique identifier of the user whose items are to be retrieved.
-        
-        Returns:
-            list: A list of items associated with the specified user.
         """
         return {"listingOfItems": []}
 
-    @router.get("/get_transaction_history/{user_id}")
-    def get_transaction_history(get_transactions: GetTransactionHistoryRequest, user_id: str) -> GetTransactionHistoryResponse:
+    @router.get("/get_transaction_history")
+    def get_transaction_history(get_transactions: GetTransactionHistoryRequest) -> GetTransactionHistoryResponse:
         """
         Retrieves a list of the user's transaction history and stores it within the user database. This includes 
         recent transactions such as buys, sells, and ISOs. Users can use this method to review their own 
         transaction history and other users' transaction history.
         
-        Parameters:
-        user_id (str): The unique identifier of the user whose transaction history is to be retrieved.
-        
-        Returns:
-            list: A list of the user's transaction history, including buys, sells, and ISOs.
         """
         return {"listingOfTransactionHistory": []}
-
-# # This is a sample GET request. Add others based on your functions!
-# @router.get("/api/profile")
-# def get_profile():
-#     '''  '''
-#     return {"listings": []}
