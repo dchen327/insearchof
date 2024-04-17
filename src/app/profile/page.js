b080053eef06d8ab668858acdd8507f10c0762bf
Arushi Malik <arushi.malik@audintel.in>
2024-04-16 20:44:39 -0700

    made changes to arushi branch

---
+++ src/app/profile/page.js
@@ -0,0 +1,309 @@
+"use client";
+import { useEffect, useState } from "react";
+import { auth, googleProvider } from "../firebase/config";
+import { signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/router";
+
+export default function Page() {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [items, setItems] = useState([]);
+  const [transactionHistory, setTransactionHistory] = useState([]);
+  const [showNameTooltip, setShowNameTooltip] = useState(false); // Added state variables for tooltips
+  const [showEmailTooltip, setShowEmailTooltip] = useState(false);
+  const [showPhoneNumberTooltip, setShowPhoneNumberTooltip] = useState(false);
+  const [showLocationTooltip, setShowLocationTooltip] = useState(false);
+  const [name, setName] = useState(""); // Added state variables for name, email, phoneNumber, and location
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [location, setLocation] = useState("");
+
+
+  // performs side effects in function components. subscribes authentication
+  // state changes when the component mounts. when authentication state changes
+  // the user state is updates and fetches the list of items and transaction
+  // history based on the specific user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        fetchListOfItems(currentUser.email);
+        fetchTransactionHistory(currentUser.email);
+      } else {
+        router.push("/");
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  // logs the user out by using the firebase's signOut method
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("signed out");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const uploadContactInformation = async () => {
+    if (!name) {
+      alert("Name is required");
+      return;
+    }
+
+    if (!email) {
+      alert("Email is required");
+      return;
+    }
+
+    if (!location) {
+      alert("Location is required");
+      return;
+    }
+
+    // Then, create a document in Firestore with the item data
+    const userData = {
+      name,
+      email,
+      phoneNumber,
+      location,
+      timestamp: new Date(), // You can use Firebase server timestamps as well
+    };
+
+    try {
+      await addDoc(itemsCollectionRef, userData);
+      alert("Request uploaded successfully!");
+
+      // Clear the form
+      setName("");
+      setEmail("");
+      setPhoneNumber("");
+      setLocation(null);
+    } catch (error) {
+      console.error("Error uploading contact information:", error);
+      alert("Failed to upload contact inforation: " + error.message);
+    }
+  };
+
+  const tooltipStyle = {
+    position: "absolute",
+    top: "100%",
+    left: "0",
+    width: "75%", // Tooltip covers 75% of the input field
+    backgroundColor: "#f0f0f0",
+    padding: "10px",
+    borderRadius: "4px",
+    boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
+    zIndex: "10",
+    marginTop: "5px",
+  };
+
+  const fetchListOfItems = async (email) => {
+    try {
+      setLoading(true);
+      const response = await fetch('/profile/get_list_of_items?requester_id=${email}');
+      if (!response.ok) {
+        throw new Error('Failed to fetch list of items');
+      }
+      setItems(response.data.listingOfItems);
+    } catch (err) {
+      console.error(err);
+      setError("Failed to fetch list of items");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchTransactionHistory = async (email) => {
+    try {
+      setLoading(true);
+      const response = await fetch('/profile/get_transaction_history?requester_id=${email}');
+      if (!response.ok) {
+        throw new Error('Failed to fetch transaction history');
+      }
+      const data = await response.json();
+      setTransactionHistory(data.listingOfTransactionHistory);
+    } catch (err) {
+      console.error(err);
+      setError("Failed to fetch transaction history");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // updates the user's profile data by taking new profile data as input
+  // and then uses updateProfile from firebase to update a user's profile
+  const updateUserProfile = async (newProfileData) => {
+    try {
+      // Update user profile data
+      await auth.currentUser.updateProfile(newProfileData);
+      // Update the user state with the new profile data
+      setUser({ ...user, ...newProfileData });
+    } catch (err) {
+      console.error(err);
+      setError("Failed to update profile");
+    }
+  };
+
+  const handleProfileUpdate = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newProfileData = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      phone_number: formData.get("phone_number"),
+      location: formData.get("location"),
+    };
+    updateUserProfile(newProfileData);
+  };
+
+  return (
+    <>
+      <div className="buttons">
+        {user ? (
+          <button className="button is-light" onClick={logOut}>
+            Log out
+          </button>
+        ) : (
+          <button className="button is-primary" onClick={signInWithGoogle}>
+            <strong>Sign In</strong>
+          </button>
+        )}
+      </div>
+
+      {["name", "email", "phoneNumber", "location"].map((field) => (
+        <div
+          key={field}
+          style={{
+            position: "relative",
+            display: "flex",
+            alignItems: "center",
+            gap: "5px",
+          }}
+        >
+          {field === "name" && (
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Name (Required)"
+              style={{
+                padding: "10px",
+                fontSize: "16px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                flexGrow: 1,
+              }}
+            />
+          )}
+          {field === "email" && (
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email (Required)"
+              style={{
+                padding: "10px",
+                fontSize: "16px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                flexGrow: 1,
+              }}
+            />
+          )}
+          {field === "phoneNumber" && (
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              placeholder="Phone Number"
+              style={{
+                padding: "10px",
+                fontSize: "16px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                flexGrow: 1,
+              }}
+            />
+          )}
+          {field === "location" && (
+            <input
+              type="text"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+              placeholder="Location (Required)"
+              style={{
+                padding: "10px",
+                fontSize: "16px",
+                border: "1px solid #ccc",
+                borderRadius: "4px",
+                flexGrow: 1,
+              }}
+            />
+          )}
+        </div>
+      ))}
+
+      <div>
+        {user && (
+          <>
+            <div>
+              <p>Welcome, {user.name}</p>
+              <p>Email: {user.email}</p>
+              <p>Phone number, {user.phone_number}</p>
+              <p>Location, {user.location}</p>
+              {/* Display profile picture if available */}
+              {user.photoURL && <img src={user.photoURL} alt="Profile" />}
+            </div>
+            <button
+              onClick={() => {
+                // Close any open tooltip and open the clicked one
+                setShowNameTooltip(
+                  field === "name" ? !showNameTooltip : false
+                );
+                setShowEmailTooltip(
+                  field === "email" ? !showEmailTooltip : false
+                );
+                setShowPhoneNumberTooltip(
+                  field === "phoneNumber" ? !showPhoneNumberTooltip : false
+                );
+                setShowLocationTooltip(
+                  field === "location" ? !showLocationTooltip : false
+                );
+              }}
+              style={{ position: "relative", zIndex: "20" }}
+            >
+              ?
+            </button>
+            {showNameTooltip && (
+              <div style={tooltipStyle}>The name of the user</div>
+            )}
+            {showEmailTooltip && field === "email" && (
+              <div style={tooltipStyle}>The email of the user</div>
+            )}
+            {showPhoneNumberTooltip && field === "phoneNumber" && (
+              <div style={tooltipStyle}>The phone number of the user</div>
+            )}
+            {showLocationTooltip && field === "location" && (
+              <div style={tooltipStyle}>
+                The location of the user for picking up requested item
+              </div>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
+}
