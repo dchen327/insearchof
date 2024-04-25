91e47ab917c3aa3daa6d0f2aacb88fa05d99f0a7
David Chen <davidc3287@gmail.com>
2024-04-24 19:42:18 -0700

    del database function

--- api/tests/catalog_test.py
+++ api/tests/catalog_test.py
@@ -1,11 +1,27 @@
 import os
+import requests
 from google.auth.credentials import AnonymousCredentials
 from google.cloud.firestore import Client
+from dotenv import load_dotenv
 
-os.environ['FIRESTORE_EMULATOR_HOST'] = 'localhost:8080'
+load_dotenv()
+
+FIREBASE_ID = os.getenv('NEXT_PUBLIC_FIREBASE_PROJECT_ID')
+FIRESTORE_EMULATORS_PORT = 'localhost:8080'
+
+os.environ['FIRESTORE_EMULATOR_HOST'] = FIRESTORE_EMULATORS_PORT
 cred = AnonymousCredentials()
 
-db = Client(project='insearchof-1fb7a', credentials=cred)
+db = Client(project=FIREBASE_ID, credentials=cred)
+
+
+def clear_db():
+    url = f"http://{FIRESTORE_EMULATORS_PORT}/emulator/v1/projects/{FIREBASE_ID}/databases/(default)/documents"
+    response = requests.delete(url)
+    if response.status_code == 200:
+        print('Database cleared')
+    else:
+        print('Error clearing database', response.status_code)
 
 
 doc_ref = db.collection("users").document("alovelace")
@@ -22,3 +38,4 @@ for doc in docs:
     print(f"{doc.id} => {doc.to_dict()}")
 
 print('hi')
+clear_db()
