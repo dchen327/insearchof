d16125f9f4c9bdce605aa760b6fb13bdfa8f8de2
David Chen <davidc3287@gmail.com>
2024-04-24 20:18:30 -0700

    setup test

---
+++ api/catalog_test.py
@@ -0,0 +1,54 @@
+import os
+import requests
+import unittest
+from google.auth.credentials import AnonymousCredentials
+from google.cloud.firestore import Client
+from dotenv import load_dotenv
+from routers.catalog import get_listings
+
+load_dotenv()
+
+FIREBASE_ID = os.getenv('NEXT_PUBLIC_FIREBASE_PROJECT_ID')
+FIRESTORE_EMULATORS_PORT = 'localhost:8080'
+
+os.environ['FIRESTORE_EMULATOR_HOST'] = FIRESTORE_EMULATORS_PORT
+cred = AnonymousCredentials()
+db = Client(project=FIREBASE_ID, credentials=cred)
+
+
+def clear_db():
+    url = f"http://{FIRESTORE_EMULATORS_PORT}/emulator/v1/projects/{FIREBASE_ID}/databases/(default)/documents"
+    response = requests.delete(url)
+    if response.status_code == 200:
+        print('Database cleared')
+    else:
+        print('Error clearing database', response.status_code)
+
+
+# doc_ref = db.collection("users").document("alovelace")
+# doc_ref.set({"first": "Ada", "last": "Lovelace", "born": 1815})
+
+# doc_ref = db.collection("users").document("aturing")
+# doc_ref.set({"first": "Alan", "middle": "Mathison",
+#             "last": "Turing", "born": 1912})
+
+# users_ref = db.collection("users")
+# docs = users_ref.stream()
+
+# for doc in docs:
+#     print(f"{doc.id} => {doc.to_dict()}")
+
+# print('hi')
+# clear_db()
+
+class CatalogTests(unittest.TestCase):
+    def setUp(self):
+        clear_db()
+
+    def test_query_empty_db(self):
+        listings = get_listings()
+        self.assertEqual(listings, {"listings": [1]})
+
+
+if __name__ == '__main__':
+    unittest.main()
